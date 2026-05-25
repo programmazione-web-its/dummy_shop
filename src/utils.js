2ed90bbb5be24340ef4285db0dd28eee6ca253bb
analayze <annalisa@antonioli.dev>
2026-05-25 16:52:22 +0200

    Add functions to utils

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,15 @@
 export function getDiscountedPrice(price, discountPercentage) {
   return Math.round((price - (price * discountPercentage) / 100) * 100) / 100
 }
+
+export function addItemToCart(cart, item) {
+  const idx = cart.findIndex((el) => el.id === item.id)
+  if (idx === -1) return [...cart, { ...item, quantity: 1 }]
+  return cart.map((el, i) =>
+    i === idx ? { ...el, quantity: el.quantity + 1 } : el,
+  )
+}
+
+export function removeItemFromCart(cart, id) {
+  return cart.filter((el) => el.id !== id)
+}
